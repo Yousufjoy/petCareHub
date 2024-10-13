84d313cddbef93269c57c02320ce478ebcdf301a
yousufjoy <yousufj93006@gmail.com>
2024-10-13 14:27:25 +0600

    fixing deployment issues

--- src/lib/connectDB.js
+++ src/lib/connectDB.js
@@ -1,9 +1,15 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 let db;
+
 export const connectDB = async () => {
   if (db) return db;
   try {
-    const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
+    const uri = process.env.MONGODB_URI; // Use the server-side environment variable
+    if (!uri) {
+      throw new Error(
+        "MongoDB connection URI is not defined in environment variables"
+      );
+    }
     const client = new MongoClient(uri, {
       serverApi: {
         version: ServerApiVersion.v1,
