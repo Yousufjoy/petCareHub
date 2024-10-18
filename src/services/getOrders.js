17b4fb1e95a9aeb3acb88c6bb7f00d604a9568c1
yousufjoy <yousufj93006@gmail.com>
2024-10-18 17:19:40 +0600

    fixing uri

--- src/services/getOrders.js
+++ src/services/getOrders.js
@@ -1,6 +1,6 @@
 export const getAllOrders = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/order/api/get-all-orders`
+    `/order/api/get-all-orders`
   ); // Await the fetch call
   const orders = await res.json(); // Await the response to parse the JSON
   return orders;
